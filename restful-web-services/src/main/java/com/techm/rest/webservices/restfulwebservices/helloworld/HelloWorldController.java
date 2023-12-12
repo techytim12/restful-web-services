577cb64d821ec17a8baa32542027de62539ce4c4
Thomas Philip <thomasphilipchry@gmail.com>
2023-12-12 12:14:53 +0530

    Internationalization

--- restful-web-services/src/main/java/com/techm/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/techm/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,10 +1,19 @@
 package com.techm.rest.webservices.restfulwebservices.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
 
     //@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
@@ -22,4 +31,36 @@ public class HelloWorldController {
         return new HelloWorldBean("Hi there, "+name);
         //return new HelloWorldBean(String.format("Hi there, %s", name));
     }
+
+    @GetMapping(path = "/hello-world-international")
+    public String helloWorldInternational(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null , "Default Message", locale);
+        //return "Hello World International";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
