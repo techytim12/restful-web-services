5604e80d0462286393fba9ba9efbaa20a7f8aeca
Thomas Philip <thomasphilipchry@gmail.com>
2023-12-12 23:51:02 +0530

    Till H2 db connected & added some data

--- restful-web-services/src/main/java/com/techm/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/techm/rest/webservices/restfulwebservices/user/User.java
@@ -1,14 +1,19 @@
 package com.techm.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
 
-
+    @Id
+    @GeneratedValue
     private int id;
     @Size(min=2, message = "Name should be at least 2 chars")
     @JsonProperty("user_name")
@@ -21,6 +26,10 @@ public class User {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
+
     }
 
     public Integer getId() {
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into user_details(birth_date, id, name)
+values(current_date(), 10001,  'Tim');
+
+insert into user_details(birth_date, id, name)
+values(current_date(), 10002,  'Cook');
+
+insert into user_details(birth_date, id, name)
+values(current_date(), 10003,  'Neo');
+
+insert into user_details(birth_date, id, name)
+values(current_date(), 10004,  'Mark');
